777b8563f8949ed71491d4ae09973dbe342a4cb9
Mayank Patel <codepirate4@gmail.com>
2020-04-26 04:05:55 +0530

    Updates

---
+++ precache-manifest.c2abd801f0f755a7bb12fce3f2776979.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "238cd3d06feabbcec99d53da238568cf",
+    "url": "/COVID19-Tracker/index.html"
+  },
+  {
+    "revision": "4246be15ba0159107492",
+    "url": "/COVID19-Tracker/static/css/main.3c71c292.chunk.css"
+  },
+  {
+    "revision": "e8428026c4cec859a2fd",
+    "url": "/COVID19-Tracker/static/js/2.9640dbb5.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID19-Tracker/static/js/2.9640dbb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4246be15ba0159107492",
+    "url": "/COVID19-Tracker/static/js/main.766ad383.chunk.js"
+  },
+  {
+    "revision": "be0bf30c3368edb3371f",
+    "url": "/COVID19-Tracker/static/js/runtime-main.4d499466.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID19-Tracker/static/media/image2.d7265326.png"
+  }
+]);
